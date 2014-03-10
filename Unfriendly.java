3e93798deae896b72e8dbcf158c6cbd19b68c019
dakshal <shahdakshal95@gmail.com>
2014-03-11 00:51:59 +0530

    Create Unfriendly.java

---
+++ Unfriendly.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Solution {
+  	private static long  gcd(long a, long b){
+		if(b==0) {return a;}
+		else {return gcd(b,a%b);}
+	}
+	public static long calc(long[]arr, long k){
+		HashSet<Long> hs= new HashSet<Long>();
+		for(int i=0;i<arr.length;i++){
+			hs.add(gcd(arr[i],k));
+		}
+		int count=0;
+		long div1;
+		long div2;
+		boolean flag=false;
+		for(int i=1;i<=Math.sqrt(k);i++){
+		    if(k%i==0){
+		    	div1=i;
+		    	div2=k/i;
+		        for(long h:hs){
+		            if(h%div1 == 0){
+		                flag=true;
+		                break;
+		            }
+		        }
+		        if(flag==false) count++;
+		        flag=false;
+		        if(div1!=div2){
+		                for(long h:hs){
+		                    if(h%div2 == 0){
+		                        flag=true;
+		                        break;
+		                    }
+		                }
+		                if(flag==false) count++;
+		                flag=false;
+		        }
+		    }
+		}
+		return count;
+	}
+    	public static void main(String[] args) throws Exception {
+		Scanner in = new Scanner(System.in);
+	        int n=in.nextInt();
+	        long k=Long.parseLong(in.next());
+	        long []arr= new long[n];
+	        for(int i=0;i<n;i++){
+	        	arr[i]=Long.parseLong(in.next());
+	        }
+	        System.out.print(calc(arr,k));
+	}
+}
